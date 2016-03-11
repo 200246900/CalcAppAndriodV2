1d6f314b164ae423dd2799f409752a165967be4c
200246900 <200246900@student.georgianc.on.ca>
2016-03-11 17:00:02 -0500

    Added Layout and Basic Fucntionality

--- app/src/main/java/com/example/human/calcappandoidv2/MainActivity.java
+++ app/src/main/java/com/example/human/calcappandoidv2/MainActivity.java
@@ -8,8 +8,45 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Variables
+    ArrayList<String> arrayList = new ArrayList<String>();
+    String string1 = "";
+    String string2 = "";
+    double firstUserInput = 0.00;
+    double secondUserInput = 0.00;
+    double answer = 0.00;
+    String calculation = "";
+    boolean stillTypingNumber = false;
+    double tempNum = 0.00;
+
+    public void onCLick1 (View v)
+    {
+        //Assign whatever the number clicked to a currentInput
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        Button button = (Button) v;
+        String string = (String) button.getText().toString();
+
+        //If the user is still typing the question
+        if (stillTypingNumber)
+        {
+            //If the user has not chosen a calculation
+            textView2.setText(string);
+        }
+        else //User is done calculation
+        {
+            //Update display with number
+            textView2.setText(string);
+            stillTypingNumber = (true);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
